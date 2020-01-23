e00f93972bab8044a0991f8aed5a9ce1841cf603
SlimyZockt <SlimyZockt77@gmail.com>
2020-01-23 10:17:35 +0100

    guns

--- scene/Camera2D.gd
+++ scene/Camera2D.gd
@@ -3,12 +3,19 @@ extends Camera2D
 const SPEED = 5
 const MAXPOS = 64
 
+var player 
+var wait = false
+
+func _ready():
+	player = get_parent().get_node("player")
+
+
 func _process(delta):
-	if get_global_mouse_position().x > 0:
-		offset.x = min(offset.x + SPEED, MAXPOS * 2)
-	elif get_global_mouse_position().x < 0:
-		offset.x = max(offset.x - SPEED, -MAXPOS * 2)
-	if get_global_mouse_position().y > 0:
+	if get_global_mouse_position().x > player.position.x:
+		offset.x = min(offset.x + SPEED, MAXPOS )
+	elif get_global_mouse_position().x < player.position.x:
+		offset.x = max(offset.x - SPEED, -MAXPOS )
+	if get_global_mouse_position().y > player.position.y:
 		offset.y = min(offset.y + SPEED, MAXPOS)
-	elif get_global_mouse_position().y < 0:
+	elif get_global_mouse_position().y < player.position.y:
 		offset.y = max(offset.y - SPEED, -MAXPOS)
--- scripts/Gun.gd
+++ scripts/Gun.gd
@@ -1,4 +1,64 @@
-extends Node
+extends Node2D
 
 class_name gun
+#class Gun:
 
+var bullet_scene
+
+var FIRE_RATE
+var BULLET_AMUONT
+var BULLET_SPEED
+var BULLET_RAIL 
+var BulletPoint
+var SHOT
+
+var can_fire = true
+const ROTATION_P = 8
+
+func _init(bullet_scene, BulletPoint, FIRE_RATE, BULLET_AMUONT, BULLET_SPEED, BULLET_RAIL, SHOT):
+	self.bullet_scene = bullet_scene
+	self.BulletPoint = BulletPoint
+	self.FIRE_RATE = FIRE_RATE
+	self.BULLET_AMUONT = BULLET_AMUONT
+	self.BULLET_SPEED = BULLET_SPEED
+	self.BULLET_RAIL = BULLET_RAIL
+	self.SHOT = SHOT
+
+func shot():
+	if can_fire:
+		for bulletR in range(BULLET_RAIL):
+			for bulletA in range( floor(-BULLET_AMUONT/2), floor(BULLET_AMUONT/2) ):
+				print(bulletA)
+				var	bullet_instance = bullet_scene.instance()
+				if is_mouse_x_y(get_global_mouse_position().x, get_global_mouse_position().y):
+					bullet_instance.position = BulletPoint.global_position + Vector2(90 * -bulletA + rotation, 0)
+				else:
+					bullet_instance.position = BulletPoint.global_position + Vector2( 0 , 90 * bulletA + rotation)
+				if is_mouse_90_axis(get_global_mouse_position().x, get_global_mouse_position().y):
+					bullet_instance.rotation_degrees = rotation_degrees + bulletA * -ROTATION_P
+				else:
+					bullet_instance.rotation_degrees = rotation_degrees + bulletA * ROTATION_P
+				bullet_instance.apply_impulse(Vector2(), Vector2(BULLET_SPEED, 0).rotated(rotation))
+				get_tree().get_root().add_child(bullet_instance)
+				SHOT.play()
+			can_fire = false
+			yield(get_tree().create_timer(FIRE_RATE), "timeout")
+			can_fire = true	
+
+func is_mouse_x_y(mouseX,mouseY):
+	var distanceX = sqrt((global_position.x - mouseX)*(global_position.x - mouseX))
+	var distanceY = sqrt((global_position.y - mouseY)*(global_position.y - mouseY)) 
+	
+	if distanceX < distanceY:
+		return true
+	else:
+		return false
+	
+func is_mouse_90_axis(mouseX,mouseY):
+	var distanceX = global_position.x - mouseX
+	var distanceY = global_position.y - mouseY
+	
+	if distanceX > distanceY:
+		return true
+	else:
+		return false
